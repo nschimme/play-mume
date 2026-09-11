eee2a7b37d9ef1ae5bf8bc0b6f6467bcb58a58dc
nschimme <5505185+nschimme@users.noreply.github.com>
2026-09-11 14:25:52 +0000

    Fix TELNET CRLF carriage return line breaks in DecafMUD display
    - Update regex in DecafMUD/src/js/decafmud.display.standard.js from /\n\r?/g to /\r\n|\r|\n/g.
    - Ensure carriage return (\r) characters are stripped during HTML line break conversion, preventing macOS WebKit/CoreText from rendering double line breaks.

--- DecafMUD/src/js/decafmud.display.standard.js
+++ DecafMUD/src/js/decafmud.display.standard.js
@@ -239,7 +239,7 @@ Display.prototype.processData = function() {
 	this._display.setAttribute('aria-busy',true);
 	
 	var span = document.createElement('span');
-	span.innerHTML = data.replace(/\n\r?/g,'<br>').replace(/> /g,'>&nbsp;')
+	span.innerHTML = data.replace(/\r\n|\r|\n/g,'<br>').replace(/> /g,'>&nbsp;')
 		.replace(/ ( +)/g, function(m) { if (m.length ===2) {return ' &nbsp;';}
 			return ' ' + new Array(m.length-1).join('&nbsp;') + ' '; });
 	this.shouldScroll();
